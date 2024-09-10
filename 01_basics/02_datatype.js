607b120ba6ddba77959c817612eefcdae4c83613
Kundan-programmer <129212364+kundan-programer@users.noreply.github.com>
2024-09-10 03:41:19 +0000

    02_DataType

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+// "use strict";  // treat all js codde as newer version
+
+// alert("hello");   // it mainly run inside the browser not in the terminal or node.js
+
+// console.log("Hello kundan ")
+
+console.log(typeof "Kundan kumar");
+
+let a = 90;
+console.log(typeof a); // return the number
+
+console.log(typeof null); // return the object
+
+console.log(typeof undefined); // return the undefined
+
+
