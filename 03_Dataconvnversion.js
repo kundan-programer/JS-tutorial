74c1a0fa6da84013a87592d56e1ad91a88b87608
Kundan-programmer <129212364+kundan-programer@users.noreply.github.com>
2024-09-10 04:16:16 +0000

    03_DataConversion

---
+++ 03_Dataconvnversion.js
@@ -0,0 +1,23 @@
+let score = "98acb";
+
+// NaN => Not a number
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let num = Number(score);
+// console.log(typeof num);
+// console.log(num);
+
+//  "33" => 33
+//  "33abc" => NaN
+//   true => 1
+//   false => 0
+//  null => 0
+//  undefined => NaN
+
+let mobNo = 8292827214;
+
+let mobNo1 = String(mobNo);
+console.log(mobNo1);
+console.log(typeof mobNo1);
