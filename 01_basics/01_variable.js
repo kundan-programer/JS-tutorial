808a88ce99f70675a34823c131b1650f66d3be84
Kundan-programmer <129212364+kundan-programer@users.noreply.github.com>
2024-09-10 03:38:57 +0000

    01_variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 1010886;
+let acconutName = "Kundan kumar";
+var accountPassword = "Kundan@123";
+var city = "Begusari";
+
+console.table([accountId, acconutName, accountPassword, city]);
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
