a854c90444fdce14249f06f4bb185a3a23f668bf
Kundan-programmer <129212364+kundan-programer@users.noreply.github.com>
2024-09-11 15:46:30 +0530

    Update 07_String.js

--- 01_basics/07_String.js
+++ 01_basics/07_String.js
@@ -1,6 +1,6 @@
 // String is the combination of character.In the JS we used both double as well as single courts to write the String.
 
-// const name="Kundan"  // ==> String
+const name = "Kundan"; // ==> String
 // const surname= 'kumar'  // ==> String
 // const id = 886;
 // // console.log(name+" "+surname);  // concatination of two string
@@ -14,8 +14,13 @@ const name2 = new String("Kundan kumar"); // this is good way to declare the ast
 
 // following are the some function which is used in the string like,
 // console.log(name2[0]); // ==> they provide the value which is present in the particular index
-// console.log(name2[1]);
-// const name1= name.toUpperCase()
-console.log(name2.toUpperCase()); // => they convert into the uppercase
-console.log(name2.toLowerCase()); // => they convert into the lowercase
+// console.log(name2.toUpperCase()); // => they convert into the uppercase
+// console.log(name2.toLowerCase()); // => they convert into the lowercase
+// console.log(name2.length); // => they give the length of the string
+// console.log(name2.charAt(1)); // it give the value of the particular index
+// console.log(name2.slice(0, 6)); // it give the value from starting to end-1
+// console.log(name2.replace("K", "D")); // basically it replace the key value
+// console.log(name2.concat(name)); // it add the new string with the privius oen
 
+
+// and also there there are many method which is used in the Strings
