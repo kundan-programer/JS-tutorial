89ebd280fe7ec52a29999b3ea44e194907fa6123
Kundan-programmer <129212364+kundan-programer@users.noreply.github.com>
2024-09-11 04:07:03 +0000

    04_comaprision

---
+++ 01_basics/04_comaprision.js
@@ -0,0 +1,14 @@
+// console.log(2>3);
+// console.log(20>3);
+// console.log(null>3);
+// console.log(+null>3);
+// console.log(undefined>3);
+// console.log(+undefined>3);
+
+let a=100;
+if(a>10){
+    console.log("Yes");    
+}
+else{
+    console.log("No");  
+}
