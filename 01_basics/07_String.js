1d6222a3905b5a92a79856f1b6c31d14e8a9d4ea
Kundan-programmer <129212364+kundan-programer@users.noreply.github.com>
2024-09-11 09:24:14 +0000

    Updated

---
+++ 01_basics/07_String.js
@@ -0,0 +1,8 @@
+// String is the combination of character.In the JS we used both double as well as single courts to write the String.
+
+const name="Kundan"  // ==> String
+const surname= 'kumar'  // ==> String
+const id = 886;
+// console.log(name+" "+surname);  // concatination of two string
+name1= ame.toUpperCase
+console.log(`Hello I am ${name1} and my id is ${id}` );
