75bb3b70bae31576cd0c9c6dd3b5e898d3664ba3
Kundan-programmer <129212364+kundan-programer@users.noreply.github.com>
2024-09-11 09:31:17 +0000

    updated

--- 01_basics/07_String.js
+++ 01_basics/07_String.js
@@ -1,8 +1,17 @@
 // String is the combination of character.In the JS we used both double as well as single courts to write the String.
 
-const name="Kundan"  // ==> String
-const surname= 'kumar'  // ==> String
-const id = 886;
-// console.log(name+" "+surname);  // concatination of two string
-name1= ame.toUpperCase
-console.log(`Hello I am ${name1} and my id is ${id}` );
+// const name="Kundan"  // ==> String
+// const surname= 'kumar'  // ==> String
+// const id = 886;
+// // console.log(name+" "+surname);  // concatination of two string
+
+// const name1= name.toUpperCase()
+// console.log(`Hello I am ${name1} and my id is ${id}`);
+
+
+
+// Another way to declare the string
+const name2=new String("Kundan kumar"); // this is good way to declare the astring bcz we have able to access in many way 
+// or this way give many way or property to use the string.
+// console.log(name2);
+
