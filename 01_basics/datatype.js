c5ad09b6f7e729aa5d71031abddf1499dcbe0552
Kundan-programmer <129212364+kundan-programer@users.noreply.github.com>
2024-09-11 08:21:34 +0000

    Updated

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -9,7 +9,29 @@ you don't need to declare the data type of a variable explicitly.
 // 1. Primitive=========================================================
 // There are seven type of primitive data type: String , number , Boolean , null , undefined, Symbol, BigInt
 
+// 1. String
+// let Str1 = "Kundan kumar";
+// console.log(Str1);
 
+// 2. number
+// let num1=8292837214;
+// console.log(num1);
+
+// 3. Boolean
+// let bool=true;
+// console.log(bool);
+
+// 4. null
+// let null1 = null;
+// console.log(null1);
+
+
+// 5. undefined
+// let und = undefined;
+// console.log(und);
+
+
+// 6. Unique no.
 // Symbol always represent unique number,eg.
 // these above both are not same
 // const id = Symbol(123);
@@ -17,6 +39,7 @@ you don't need to declare the data type of a variable explicitly.
 // console.log(id == anotherid);
 
 
+// 7. BigInt
 // const BigNumber = 9090090909089090907906234567878;
 // console.log(BigNumber);
 // console.log(typeof BigNumber)
@@ -25,3 +48,23 @@ you don't need to declare the data type of a variable explicitly.
 
 // 2. Non-Primitive ============================================
 // Array, objects , functions
+
+// 1. Arrays
+// const Arr1=["kundan","Harsh","Piyush","Utkarsh"]
+// console.log(Arr1)
+
+// 2.Objects
+// let obj =
+// {
+//     Name:"Kundan",
+//     Age: 20,
+//     Branch:"BTech"
+// }
+// console.log(obj);
+
+// 3. Function
+// const myfun = function(){
+// console.log("Hello Kundan");
+// }
+
+// myfun()   // calling the function
