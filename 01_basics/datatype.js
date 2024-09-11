e7a41423f44e2dc94aca591b8a98a13924ebabad
Kundan-programmer <129212364+kundan-programer@users.noreply.github.com>
2024-09-11 06:00:01 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,13 @@
+/*
+Note:- JavaScript is a dynamic language and not static, 
+which means that variables can hold values of different types during runtime. 
+Unlike languages such as Typescript or Java,
+you don't need to declare the data type of a variable explicitly.
+*/
+
+// Data type => There are two type of data type:
+// 1. Primitive
+// There are seven type of primitive data type: String , number , Boolean , null , undefined, Symbol, BigInt
+
+// 2. Non-Primitive
+// Array, objects , functions
